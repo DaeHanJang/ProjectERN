2f693b9e3e5d7b2b3fa985cffc62b33c7110487a
BlueBaek <sktjr0472@gmail.com>
2026-04-22 18:15:52 +0900

    Feat: 플레이어캐릭터용 ABP 생성 및 BP적용
    - ABP에 적용시킬 C++ 클래스를 만들고 이를 상속받은 ABP 파일 생성
    - 전사 캐릭터로 사용할 BP파일 생성

---
+++ Source/ProjectERN/Character/Animation/ERNPlayerAnimInst.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/Animation/ERNPlayerAnimInst.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UERNPlayerAnimInst::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	// 소유자 캐릭터 가져오기 (PawnOwner는 애니메이션 인스턴스가 붙은 캐릭터)
+	ACharacter* Char = Cast<ACharacter>(TryGetPawnOwner());
+	if (!Char)
+	{
+		return;  // 캐릭터가 없으면 업데이트하지 않음
+	}
+	
+	// MovementComponent 가져오기
+	UCharacterMovementComponent* MovementComp = Char->GetCharacterMovement();
+	if (!MovementComp)
+	{
+		return;
+	}
+	
+	// 속도 계산 (멀티플레이에서 Velocity는 서버 동기화됨)
+	Velocity = MovementComp->Velocity;
+	Speed = Velocity.Size();
+	
+	// 공중 확인 (점프/낙하)
+	bIsInAir = MovementComp->IsFalling();
+	
+	// 가속 확인
+	bIsAccelerating = MovementComp->GetCurrentAcceleration().Size() > 0.f;
+	
+	// 이동 방향 계산 (캐릭터의 회전 기준으로 상대적 방향)
+	if (Speed > 0.f)
+	{
+		FRotator CharRotation = Char->GetActorRotation();
+		FRotator VelocityRotation = Velocity.Rotation();
+		Direction = FMath::UnwindDegrees(VelocityRotation.Yaw - CharRotation.Yaw);
+	}
+	else
+	{
+		// 정지 시 방향 0
+		Direction = 0.f;
+	}
+}
