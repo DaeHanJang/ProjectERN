8206b246df61e4f84e4890c8cd10fe4acebf64cb
Earthstarr <yjw4114@gmail.com>
2026-06-11 18:22:53 +0900

    #282 fix-점프시 즉사기 패턴 버그
    * 트리가드 밸패
    
    - 3,4 페이즈에서 받는피해감소가 삭제되었습니다 : (35% 감소 -> 0% 감소)
    - 즉사기 패턴의 피해량이 감소합니다 : (체력 200% -> 체력 50%)
    - 유도 번개의 피해량이 감소합니다 : (체력 2% -> 체력 1%)
    - 비유도 번개의 피해량이 감소합니다 : (체력 10% -> 체력 5%)
    - 긴 유도 번개의 지속 시간이 감소합니다 : (8s -> 5s)
    - 짧은 유도 번개의 지속 시간이 감소합니다 : (3s -> 1.5s)
    - 3페이즈에 약화된 즉사기(체력 10%) 패턴이 추가됩니다
    - 즉사기의 전조와 데미지 판정 사이의 간격이 증가합니다
    
    * 점프때 즉사기 타격시 점프 어빌리티 버그 수정

--- Source/ProjectERN/Character/Enemy/ERNBossCharacter.cpp
+++ Source/ProjectERN/Character/Enemy/ERNBossCharacter.cpp
@@ -75,7 +75,7 @@ float AERNBossCharacter::TakeDamage(float DamageAmount, FDamageEvent const& Dama
 	// 3페이즈 이상이면 받는 데미지 감소
 	if (CurrentPhaseIndex >= 2)
 	{
-		DamageAmount *= 0.65f;
+		DamageAmount *= 0.8f;
 	}
 
 	const float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
